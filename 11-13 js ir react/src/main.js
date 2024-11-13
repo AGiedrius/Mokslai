b8c901f22f8ffbc94dd20779cf4f114d32cf86bc
AGiedrius <agiedrius@outlook.com>
2024-11-13 12:35:29 +0200

    JS from 11-13

---
+++ 11-13 js ir react/src/main.js
@@ -0,0 +1,39 @@
+const num1 = 0;
+const num2 = 0;
+const operation = "";
+let err = "";
+let rez = 0;
+const calculate = (num1, num2, operation) => {
+  try {
+    if (
+      operation != "+" &&
+      operation != "-" &&
+      operation != "*" &&
+      operation != "/"
+    ) {
+      throw new Error("Invalid operation");
+    }
+    switch (operation) {
+      case "+":
+        rez = num1 + num2;
+        break;
+      case "-":
+        rez = num1 - num2;
+        break;
+      case "*":
+        rez = num1 * num2;
+        break;
+      case "/":
+        if (num2 === 0) {
+          throw new Error("Cannot divide by zero");
+        } else rez = num1 / num2;
+        break;
+    }
+    return rez;
+  } catch (err) {
+    // console.log(err);
+    console.error(err);
+    // alert(error.mesage);
+  }
+};
+console.log(calculate(100, 33, "a"));
