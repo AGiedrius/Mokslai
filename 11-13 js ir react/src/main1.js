3af5ccc128b2f90a412de000eb1920bebf777adf
AGiedrius <agiedrius@outlook.com>
2024-11-14 08:37:18 +0200

    JS 4 from 11-13

--- 11-13 js ir react/src/main1.js
+++ 11-13 js ir react/src/main1.js
@@ -1,36 +1,19 @@
-// const fetchPokemonAPI = (id) =>
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//       .then((res) => res.json())
-//       .then((data) => console.log(data))
-//       .catch((err) => console.error(err));
-
-//   const fetchPokemon = async (id) => {
-//     try {
-//       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-//       const data = await res.json();
-//       console.log(data);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   fetchPokemon(5000);
-//   fetchPokemonAPI(5000);
-
 const fetchUserData = async (id) => {
   try {
     const resurs = await fetch(
       `https://jsonplaceholder.typicode.com/users/${id}`
     );
     const data = await resurs.json();
-    if (!resurs.ok) {
+    if (resurs.status === 404) {
       throw new Error("User not found");
     } else {
       console.log("User Name:  " + data.name);
     }
     // catch {if (resurs = instanceof)}
   } catch (error) {
-    console.error(error);
+    if (error instanceof TypeError) {
+      throw new Error("Network kaput");
+    } else console.error(error);
   }
 };
-fetchUserData(999);
+fetchUserData(9);
